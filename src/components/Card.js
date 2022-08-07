09c98c10f02b6ab212fc9f12aa1e74d6b5dac1a9
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-07 14:37:59 +0200

    setup user and followers sections in the dashboard page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+
 const Card = () => {
-  return <h2>card component</h2>;
+  const {githubUser} = useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name, 
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser
+  
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'John Doe'}</p>
+        </div>
+        <a href={html_url}>Follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness />
+          {company}
+        </p>
+        <p>
+          <MdLocationOn />
+          {location || 'Planet Earth'}
+        </p>
+        <a href={`https://${blog}`}>
+          <MdLink />
+          {blog}
+        </a>
+      </div>
+    </Wrapper>
+  );
 };
+
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
