f6d9f3482d56b88a80770e5064e8db46f8d48357
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-07 14:06:49 +0200

    setup 404 page and info component in the dashboard page

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, createContext, useContext} from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+export const GithubContext = createContext(); 
+
+export const GithubProvider = ({children}) => {
+  const [githubUser, setGithubUser] = useState(mockUser); 
+  const [repos, setRepos] = useState(mockRepos); 
+  const [followers, setFollowers] = useState(mockFollowers); 
+  
+  
+  
+  
+  const value = {
+    githubUser, 
+    repos, 
+    followers, 
+  }
+  
+  return (
+    <GithubContext.Provider value={value}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(GithubContext); 
+}
