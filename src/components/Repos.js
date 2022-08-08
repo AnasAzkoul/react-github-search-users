7b605ab870c293e933a5965dd78601ad9c61be05
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-08 12:08:38 +0200

    finalized the charts for the repos component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,48 +5,59 @@ import {ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D} from './Charts';
 
 const Repos = () => {
   const {repos} = useContext(GithubContext);
-  console.log(repos);
+
   
   let languages = repos.reduce((total, item) => {
-    const {language} = item; 
+    const { language, stargazers_count } = item; 
     if (!language) return total; 
     if (!total[language]) {
-      total[language] = {label: language, value: 1}
+      total[language] = {label: language, value: 1, stars: stargazers_count}
     } else {
-      total[language] = {...total[language], value: total[language].value + 1}
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1, 
+        stars: total[language].stars + stargazers_count
+      }
     }
     return total
   }, {})
   
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
     return b.value - a.value
     })
     .slice(0, 5)
   
-  console.log(languages);
-
-  // STEP 2 - Chart Data
-  // const chartData = [
-  //   {
-  //     label: '',
-  //     value: '',
-  //   },
-  //   {
-  //     label: '',
-  //     value: '',
-  //   },
-  //   {
-  //     label: '',
-  //     value: '',
-  //   },
-  // ];
+  // most stars per language
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => b.stars - a.stars)
+    .map(item => {return {...item, value: item.stars}})
+    .slice(0, 5)
+  
+  // stars, forks 
+  let {stars, forks} = repos.reduce((total, item) => {
+    const {stargazers_count, name, forks} = item; 
+    total.stars[stargazers_count] = {label: name, value: stargazers_count}
+    total.forks[forks] = {label: name, value: forks}
+    return total
+  }, {
+    stars: {}, 
+    forks: {}, 
+  })
+  
+  stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
+  
+  console.log(stars);
+  console.log(forks);
 
   return (
     <section className='section'>
       <Wrapper className='section-center'>
-        {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={languages}/>
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars}/>
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks}/>
       </Wrapper>
     </section>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import {GithubContext} from '../context/context';
+
+
+
 const Search = () => {
   return <h2>search component</h2>;
 };
